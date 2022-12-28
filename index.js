58a936cac40deb17728a4af55d7404bdfb67dda2
KorrectionUncelp <121499226+KorrectionUncelp@users.noreply.github.com>
2022-12-28 13:08:32 +0300

    See README

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var port = data["port"];
-var username = data["name"]
+var host = process.env['AFK_IP'] || data["ip"];
+var port = process.env['AFK_PORT'] || data["port"];
+var username = process.enc['AFK_NAME'] || data["name"]
 var bot = mineflayer.createBot({
   host: host,
   port: port,
